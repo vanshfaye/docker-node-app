cf9391d71e25d185da0334a7d645cc01f8036786
vanshfaye <vanshfaye91@gmail.com>
2026-04-15 15:31:11 +0530

    fix binding issue

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send("Hello from DevOps Project 🚀");
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
